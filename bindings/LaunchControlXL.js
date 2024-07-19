7ec123623de571601189f259cb51e226ab7807cb
agrippa kellum <ask252@cornell.edu>
2024-07-19 18:40:10 -0400

    launch control bindings

---
+++ bindings/LaunchControlXL.js
@@ -0,0 +1,61 @@
+/** KNOBS */
+// Send A
+const k1A = "Launch Control XL:0xb0:0xd";
+const k2A = "Launch Control XL:0xb0:0xe";
+const k3A = "Launch Control XL:0xb0:0xf";
+const k4A = "Launch Control XL:0xb0:0x10";
+const k5A = "Launch Control XL:0xb0:0x11";
+const k6A = "Launch Control XL:0xb0:0x12";
+const k7A = "Launch Control XL:0xb0:0x13";
+const k8A = "Launch Control XL:0xb0:0x14";
+
+// Send B
+const k1B = "Launch Control XL:0xb0:0x1d";
+const k2B = "Launch Control XL:0xb0:0x1e";
+const k3B = "Launch Control XL:0xb0:0x1f";
+const k4B = "Launch Control XL:0xb0:0x20";
+const k5B = "Launch Control XL:0xb0:0x21";
+const k6B = "Launch Control XL:0xb0:0x22";
+const k7B = "Launch Control XL:0xb0:0x23";
+const k8B = "Launch Control XL:0xb0:0x24";
+
+// Pan / Device
+const k1C = "Launch Control XL:0xb0:0x31";
+const k2C = "Launch Control XL:0xb0:0x32";
+const k3C = "Launch Control XL:0xb0:0x33";
+const k4C = "Launch Control XL:0xb0:0x34";
+const k5C = "Launch Control XL:0xb0:0x35";
+const k6C = "Launch Control XL:0xb0:0x36";
+const k7C = "Launch Control XL:0xb0:0x37";
+const k8C = "Launch Control XL:0xb0:0x38";
+
+/** SLIDERS */
+const s1 = "Launch Control XL:0xb0:0x4d";
+const s2 = "Launch Control XL:0xb0:0x4e";
+const s3 = "Launch Control XL:0xb0:0x4f";
+const s4 = "Launch Control XL:0xb0:0x50";
+const s5 = "Launch Control XL:0xb0:0x51";
+const s6 = "Launch Control XL:0xb0:0x52";
+const s7 = "Launch Control XL:0xb0:0x53";
+const s8 = "Launch Control XL:0xb0:0x54";
+
+/** BUTTONS (button down) */
+// Track Focus
+const bf1 = "Launch Control XL:0x90:0x29";
+const bf2 = "Launch Control XL:0x90:0x2a";
+const bf3 = "Launch Control XL:0x90:0x2b";
+const bf4 = "Launch Control XL:0x90:0x2c";
+const bf5 = "Launch Control XL:0x90:0x39";
+const bf6 = "Launch Control XL:0x90:0x3a";
+const bf7 = "Launch Control XL:0x90:0x3b";
+const bf8 = "Launch Control XL:0x90:0x3c";
+
+// Track control
+const bc1 = "Launch Control XL:0x90:0x49";
+const bc2 = "Launch Control XL:0x90:0x4a";
+const bc3 = "Launch Control XL:0x90:0x4b";
+const bc4 = "Launch Control XL:0x90:0x4c";
+const bc5 = "Launch Control XL:0x90:0x59";
+const bc6 = "Launch Control XL:0x90:0x5a";
+const bc7 = "Launch Control XL:0x90:0x5b";
+const bc8 = "Launch Control XL:0x90:0x5c";
